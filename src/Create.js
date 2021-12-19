a2e73430ac0f8d1506ff55a8fa559ee43dcf09d6
Sudhanshusoni45 <sudhanshusoni45@gmail.com>
2021-12-19 12:38:53 +0530

    updates

---
+++ src/Create.js
@@ -0,0 +1,10 @@
+const Create = () => {
+  return (
+    <div className="create">
+      <h2>Add a new Blog</h2>
+      {console.log("create rendered")}
+    </div>
+  );
+};
+
+export default Create;
---
+++ src/useFetch.js
@@ -0,0 +1,42 @@
+import { useState, useEffect } from "react";
+
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const abortCont = new AbortController();
+    console.log("use effect ran");
+
+    setTimeout(() => {
+      fetch(url, { signal: abortCont.signal })
+        .then((res) => {
+          if (!res.ok) {
+            throw Error("could not fetch the data for that resource");
+          }
+          return res.json();
+        })
+        .then((data) => {
+          setData(data);
+          setIsPending(false);
+          setError(null);
+        })
+        // The catch method does not work if we reach the server and the server is still sending a response object for example if we are trying to fetch a end point that does not exist, the server will still send a response object therefore the .catch won't catch anything. catch works in the case when we are not even able to reach the server.
+        .catch((err) => {
+          if (err.name === "AbortError") {
+            console.log("fetch aborted");
+          } else {
+            setError(err.message);
+            setIsPending(false);
+          }
+        });
+    }, 1000);
+
+    return () => abortCont.abort();
+  }, [url]);
+
+  return { data, isPending, error };
+};
+
+export default useFetch;
